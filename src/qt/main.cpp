75f7516b511d2522375a679e15f609c10bc962b8
ksoze95 <ksoze95@tutanota.com>
2023-01-31 22:35:19 +0000

    Updated the qt audacity application to load the AudacityIcon ...
    ... font and added some debug logging.

--- src/qt/main.cpp
+++ src/qt/main.cpp
@@ -1,18 +1,29 @@
 /*  SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include <QtQml>
+#include <QtCore/QDebug>
+#include <QtGui/QFontDatabase>
+#include <QtQml/QQmlApplicationEngine>
 #include <QtWidgets/QApplication>
-#include <QFileInfo>
 
 int main(int argc, char *argv[])
 {
    QGuiApplication app(argc, argv);
    QQmlApplicationEngine engine;
 
+   int status = QFontDatabase::addApplicationFont(":/fonts/AudacityIcon.ttf");
+   if (status != 0)
+   {
+      qDebug() << "Unable add AudacityIcon.ttf application font";
+      return -1;
+   }
+
    engine.load("qrc:/qml/main.qml");
 
    if (engine.rootObjects().isEmpty())
+   {
+      qDebug() << "Unable to load main.qml";
       return -1;
+   }
 
    return app.exec();
 }
